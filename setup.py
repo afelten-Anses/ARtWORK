0f7ea82c16553ca16ea174b19edbb55935de05af
MALLET Ludovic <l.mallet@SAS-PP-LSCALC3.afssa.fr>
2018-11-23 10:51:34 +0100

    modifications initiale des scripts pour le packaging conda. ajout des wrappers minimaux pour bbmap et bbnorm

---
+++ setup.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python2
+
+          
+import setuptools 
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ARtWORK",
+    version="1.0.0",
+    author="Arnaud Felten, Kevin Durimel, Ludovic Mallet (pack)",
+    author_email="arnaud.felten@anses.fr, ludovic.mallet@anses.fr",
+    description="Assembly of Reads and Typing Workflow",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/afelten-Anses/ARtWORK",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Programming Language :: Python :: 3.4",
+        "Operating System :: POSIX :: Linux",
+    ],
+        scripts=['src/ARtWORK',
+             "src/ARtWORK_Assembler",
+             "src/ARtWORK_lite",
+             "src/MetARtWORK",
+             "src/bbmap",
+             "src/bbnorm",
+             ],
+    include_package_data=True,
+    install_requires=['biopython>=1.68', 
+                      'lxml',
+                      'requests',
+                      'pymongo',
+                      'bson',
+                      "featmongo",
+                      "pyunpack",
+                      "patoolib",
+                      "networkx",
+                      "numpy",
+                      ],
+    zip_safe=False,
+
+
+
+
+
+
+
+)
